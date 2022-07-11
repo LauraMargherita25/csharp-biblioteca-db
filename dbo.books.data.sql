e6904e3821e22d22411b3ded92063be454a36a71
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-07-12 00:29:19 +0200

    Add Data

---
+++ dbo.books.data.sql
@@ -0,0 +1,3 @@
+﻿SET IDENTITY_INSERT [dbo].[books] ON
+INSERT INTO [dbo].[books] ([Id], [title], [author], [year], [plot]) VALUES (NULL, NULL, NULL, NULL, NULL)
+SET IDENTITY_INSERT [dbo].[books] OFF
